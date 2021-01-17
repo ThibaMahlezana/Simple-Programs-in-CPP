15053a7c61bd4ff43f2b2e24f703cfbb7005caa2
Ayttiq Technologies <thiba.ma@gmail.com>
2021-01-17 23:41:34 +0200

    added files

---
+++ add_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num_1, num_2, sum;
+
+    cout << "Enter the numbers: ";
+    cin >> num_1 >> num_2;
+
+    sum = num_1 + num_2;
+    cout << num_1 << " + " << num_2 << " = " << sum;
+
+    return 0;
+}
